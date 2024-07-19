f545760c3d8fb42330ef20cbd4b75a8c44f02117
alanasilva88 <alanasilva.as@gmail.com>
2024-07-19 09:23:32 -0300

    Logo

--- book/src/App.jsx
+++ book/src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/notfound/NotFound";
+import Login from "./pages/login/Login";
+import { Toaster } from "react-hot-toast";
+
+
 
 function App() {
-
-  return (
+  return(
     <>
-   
+      <BrowserRouter>
+        {/* <Menu /> */}
+        <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<Login />} />
+
+        </Routes>
+        {/* <Rodape /> */}
+      </BrowserRouter>
+      <Toaster position="bottom-right" />
     </>
-  )
+  );
 }
 
-export default App
-
-//Alana
+export default App;

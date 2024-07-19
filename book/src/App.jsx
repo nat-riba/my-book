9e5d0d381e59c3fc9628c77673308c22e59c6027
Nathália Ribeiro <nat_riba@hotmail.com>
2024-07-19 09:25:51 -0300

    pagina de ajuda

--- book/src/App.jsx
+++ book/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import React from 'react';
+import Ajuda from './components/ajuda/Ajuda';
 
 function App() {
 
   return (
     <>
-   
+        <div className="app">
+          <Ajuda/>
+        </div>
     </>
-  )
+  );
 }
 
 export default App

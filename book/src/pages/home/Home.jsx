54ef9637d54ce71aca390128f271517b29dcf1e1
Nathália Ribeiro <nat_riba@hotmail.com>
2024-07-22 09:36:46 -0300

    Home

--- book/src/pages/home/Home.jsx
+++ book/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import Catalogo from "../catalog/Catalog.jsx";
+import "./Home.css";
+import {Button} from "react-bootstrap";
 
 function Home() {
     return(
         <main>
-            <h1>Home</h1>
-            <Catalogo />
+            <h1>Seja Bem vindo a nossa livraria !!</h1>
+            <h2>Aqui você encontra os melhores livros, dos melhores autores, tudo em um só lugar!</h2>
+            <p>Veja nosso catálogo de livros</p>
+            <Button>Catálogo</Button>
         </main>
     );
 }

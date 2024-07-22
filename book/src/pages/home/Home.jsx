eea52ae7c6d83431be8d27862e4f64b3f6adab25
Nathália Ribeiro <nat_riba@hotmail.com>
2024-07-22 12:34:18 -0300

    Home

--- book/src/pages/home/Home.jsx
+++ book/src/pages/home/Home.jsx
@@ -1,14 +1,17 @@
-import Catalogo from "../catalog/Catalog.jsx";
 import "./Home.css";
-import {Button} from "react-bootstrap";
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Home() {
     return(
-        <main>
-            <h1>Seja Bem vindo a nossa livraria !!</h1>
+        <main className="home">
+            <h1>Bem vindo ao MyBook, a sua livraria online!</h1>
             <h2>Aqui você encontra os melhores livros, dos melhores autores, tudo em um só lugar!</h2>
             <p>Veja nosso catálogo de livros</p>
-            <Button>Catálogo</Button>
+            <Link to="/Catalog" className='text-decoration-none'> 
+            <Button variant="outline-dark" className="mt-2 w-80 shadow-lg">Catálogo</Button></Link>
+                    
         </main>
     );
 }

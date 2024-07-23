2127884f7f9777f83e04080e6fcf3937a89bb9ef
alanasilva88 <alanasilva.as@gmail.com>
2024-07-23 06:05:26 -0300

    Correção, cadastro

--- book/src/pages/cadastro/Cadastro.jsx
+++ book/src/pages/cadastro/Cadastro.jsx
@@ -1,116 +1,88 @@
-import { useContext, useEffect, useState } from "react";
-import { getBooksByUser, deleteBook } from "../../firebase/bookService";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { Card, Container, Badge, Button } from "react-bootstrap";
-import { UsuarioContext } from "../../contexts/UsuarioContext";
-import toast from "react-hot-toast";
-import Loader from "../../components/loader/Loader";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { cadastrarUsuario, entrarGoogle } from "../../firebase/auth";
+import { Button } from "react-bootstrap";
+import toast from "react-hot-toast" ;
 import "./Cadastro.css";
 
-function Catalog() {
-    const [books, setBooks] = useState([]);
-    const [favoritos, setFavoritos] = useState([]);
-    const usuario = useContext(UsuarioContext);
+
+
+function Cadastro() {
+    const { register, handleSubmit, formState: { errors } } = useForm ();
     const navigate = useNavigate();
 
-    function carregarDados() {
-        if (usuario) {
-            getBooksByUser(usuario.uid).then((resultados) => {
-                setBooks(resultados);
-            });
-        }
-    }
+    const cadastrar = (data) => {
+        cadastrarUsuario(data.nome, data.email, data.senha).then(() => {
+            toast.success(`Bem-vindo (a)! ${data.nome}`);
+            navigate("/catalogo");
+        }).catch((error) => {
+            toast.error("Um erro inesperado aconteceu!");
+        });
+    } 
 
-    function delBook(id) {
-        const deletar = confirm("Tem certeza?");
-        if (deletar) {
-            deleteBook(id).then(() => {
-                toast.success("Livro removido com sucesso!");
-                carregarDados();
-            }).catch(() => {
-                toast.error("Erro ao remover o livro");
-            });
-        }
-    }
-
-    function toggleFavorito(id) {
-        setFavoritos(prevFavoritos => {
-            if (prevFavoritos.includes(id)) {
-                return prevFavoritos.filter(favId => favId !== id);
-            } else {
-                return [...prevFavoritos, id];
-            }
+    const handleEntrarGoogle = () => {
+        entrarGoogle().then(() => {
+         toast.success("Bem-vindo (a)!");
+         navigate("/catalogo");
         });
     }
 
-    useEffect(() => {
-        carregarDados();
-    }, []);
-
-    if (usuario === null) {
-        return <Navigate to="/login" />;
-    }
-
-    const genero = {
-        "Romance": "primary",
-        "Fantasia": "warning",
-        "Ficção": "success",
-        "Religioso": "info",
-        "outros": "danger",
-    };
-
-    return (
-        <main>
-            <h1 className="text-center">Seu catálogo de livros!</h1>
-            <h2>Sua biblioteca, suas regras:</h2>
-            <p><strong>Solicite aqui novos títulos:</strong></p>
-            <Link className="btn btn-outline-dark my-1 w-80 shadow-lg" to="/catalog/add">Solicite seu livro</Link>
-            <hr />
-            {books.length > 0 ? (
-                <Container className="mt-5">
-                    {books.map(book => (
-                        <section key={book.id} className="my-2 p-1">
-                            <Card className="my-2 p-3">
-                                <Card.Title className="Card-Title">{book.titulo}</Card.Title>
-                                <Card.Text>
-                                    <p><strong>Autor:</strong> {book.autor}</p>
-                                    <p><strong>Gênero:</strong> {book.genero}</p>
-                                    <p><strong>Ano:</strong> {book.ano}</p>
-                                </Card.Text>
-                                <div>
-                                    {book.concluido ? (
-                                        <Badge bg="success" className="m-1">Concluído</Badge>
-                                    ) : (
-                                        <Badge bg="warning" className="m-1">Pendente</Badge>
-                                    )}
-                                    <Badge bg={genero[book.genero] || "secondary"} className="m-1">{book.genero}</Badge>
-                                </div>
-                                <Button
-                                    variant="outline-dark m-1"
-                                    onClick={() => navigate(`/catalog/edit/${book.id}`)}
-                                >
-                                    Editar
-                                </Button>
-                                <Button
-                                    variant="outline-danger m-1"
-                                    onClick={() => delBook(book.id)}
-                                >
-                                    Excluir
-                                </Button>
-                                <Button
-                                    variant={favoritos.includes(book.id) ? "warning" : "outline-warning"}
-                                    className="m-1"
-                                    onClick={() => toggleFavorito(book.id)}
-                                >
-                                    {favoritos.includes(book.id) ? "Desfavoritar" : "Favoritar"}
-                                </Button>
-                            </Card>
-                        </section>
-                    ))}
-                </Container>
-            ) : <Loader />}
+    return(
+        <main className="main-cadastro">
+            <form className="form-section shadow-lg" onSubmit={handleSubmit(cadastrar)}>
+                <h1>Faça seu cadastro com:</h1>
+                <Button variant="outline-danger" className="mt-3 w-100 shadow-lg" type="button" onClick={handleEntrarGoogle}>
+                    Google
+                </Button>
+                <h1>Ou preencha suas informações: </h1>
+                <hr />
+                <div>
+                    <label htmlFor="nome">Seu nome:</label>
+                    <input 
+                        type="text"
+                        id="nome"
+                        className="form-control"
+                        maxLength={50}
+                        placeholder="Digite seu nome"
+                        {...register("nome", {required: "Nome inválido!"})}
+                    />
+                    {errors.nome && <small className="invalid">{errors.nome.message}</small>}
+                </div>
+                <div>
+                    <label htmlFor="email">Seu email:</label>
+                    <input 
+                        type="text"
+                        id="email"
+                        className="form-control"
+                        placeholder="Digite seu email"   
+                        {...register("email", {required: "Email inválido, tente novamente!", 
+                            pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/, 
+                            message: "Email inválido"}})
+                        }                     
+                    />
+                    {errors.email && <small className="invalid">{errors.email.message}</small>}
+                </div>
+                <div>
+                    <label htmlFor="senha">Sua senha:</label>
+                    <input 
+                        type="password"
+                        id="senha"
+                        className="form-control"
+                        placeholder="Digite sua senha"
+                        {...register("senha", {required: "Campo obrigatório", 
+                            minLength: {value: 6, message: "Mínimo de 6 caracteres!"}})
+                        }
+                    />
+                    {errors.senha && <small className="invalid" >{errors.senha.message}</small>}
+                </div>
+                <Button className="mt-5 custom-button" type="submit">
+                    Cadastrar
+                </Button>
+                
+            </form>
         </main>
+
     );
 }
 
-export default Catalog;
+export default Cadastro;
